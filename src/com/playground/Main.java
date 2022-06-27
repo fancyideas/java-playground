3a33572e27106ccca77e186d5109b90cc971a969
Gaowy <gaowy.dev@outlook.com>
2022-06-27 22:10:28 +0800

    init

--- src/com/playground/Main.java
+++ src/com/playground/Main.java
@@ -2,13 +2,14 @@ package com.playground;
 
 public class Main {
     public static void main(String[] args) {
-//        boolean result = compute01(4) && compute02(1) && compute03(5);
+        // 不会触发短路 &&
         boolean result01 = compute01(4);
         boolean result02 = compute02(1);
         boolean result03 = compute03(5);
-
-//        System.out.println(result);
         System.out.println(result01 && result02 && result03);
+        // 触发短路 &&
+//        boolean result = compute01(4) && compute02(1) && compute03(5);
+//        System.out.println(result);
     }
 
     private static boolean compute01(int arg) {
